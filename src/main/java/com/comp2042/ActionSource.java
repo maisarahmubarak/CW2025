e67227f4c65bf246addc70a89e0ce67217ffde50
Maisarah <maisarahmubarak09@gmail.com>
2025-12-07 22:44:45 +0800

    Remove deprecated input handling classes and interfaces to streamline codebase and improve maintainability

--- src/main/java/com/comp2042/ActionSource.java
+++
@@ -1,8 +0,0 @@
-package com.comp2042;
-
-/**
- * Replacement enum for EventSource. Use ActionSource.USER or ActionSource.THREAD.
- */
-public enum ActionSource {
-    USER, THREAD
-}
--- src/main/java/com/comp2042/ActionType.java
+++
@@ -1,8 +0,0 @@
-package com.comp2042;
-
-/**
- * Replacement enum for EventType. Use ActionType.DOWN/LEFT/RIGHT/ROTATE.
- */
-public enum ActionType {
-    DOWN, LEFT, RIGHT, ROTATE
-}
