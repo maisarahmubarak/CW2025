aa5cf1d891bd30bc897e1b9f46c0a7dd2b600059
Maisarah <maisarahmubarak09@gmail.com>
2025-12-07 23:53:50 +0800

    Remove deprecated EventSource and EventType enums, and delete unused background image

--- src/main/java/com/comp2042/EventSource.java
+++
@@ -1,9 +0,0 @@
-package com.comp2042;
-
-/**
- * @deprecated Replaced by {@link ActionSource}. Kept for backward compatibility.
- */
-@Deprecated
-public enum EventSource {
-    USER, THREAD
-}
--- src/main/java/com/comp2042/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.comp2042;
-
-/**
- * @deprecated Replaced by {@link ActionType}. Kept for backward compatibility.
- */
-@Deprecated
-public enum EventType {
-    DOWN, LEFT, RIGHT, ROTATE
-}
